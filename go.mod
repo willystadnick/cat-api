d7360798fb8ea4a1beb5b7e3181a75a3f6078b61
Willy Stadnick <willy.stadnick@gmail.com>
2021-01-11 22:44:57 -0300

    Add basic db setup

--- go.mod
+++ go.mod
@@ -5,4 +5,6 @@ go 1.15
 require (
 	github.com/gin-gonic/gin v1.6.3
 	github.com/stretchr/testify v1.6.1
+	gorm.io/driver/mysql v1.0.3
+	gorm.io/gorm v1.20.11
 )
--- main.go
+++ main.go
@@ -1,16 +1,32 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+    "github.com/gin-gonic/gin"
+    "gorm.io/driver/mysql"
+    "gorm.io/gorm"
+)
 
 func setupRouter() *gin.Engine {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(200, "pong")
-	})
-	return r
+    r := gin.Default()
+    r.GET("/ping", func(c *gin.Context) {
+        c.String(200, "pong")
+    })
+    return r
+}
+
+type Cat struct {
+    gorm.Model
+    Name string
 }
 
 func main() {
-	r := setupRouter()
-	r.Run(":8080")
+    db, err := gorm.Open(mysql.Open("cat-api:cat-api@tcp(127.0.0.1:3306)/cat-api?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
+    if err != nil {
+        panic("failed to connect database")
+    }
+
+    db.AutoMigrate(&Cat{})
+
+    r := setupRouter()
+    r.Run(":8080")
 }
