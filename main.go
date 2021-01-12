a8f0612c28150fb98d3637a7b0f8a26a9e8a9f27
Willy Stadnick <willy.stadnick@gmail.com>
2021-01-12 19:30:32 -0300

    Add cat search

--- main.go
+++ main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
+    "encoding/json"
+    "io/ioutil"
+    "net/http"
+
     "github.com/gin-gonic/gin"
     "gorm.io/driver/mysql"
     "gorm.io/gorm"
 )
 
-func setupRouter() *gin.Engine {
-    r := gin.Default()
-    r.GET("/ping", func(c *gin.Context) {
-        c.String(200, "pong")
-    })
-    return r
+type Api struct {
+    Database *gorm.DB
+    Router *gin.Engine
 }
 
 type Cat struct {
@@ -20,6 +21,14 @@ type Cat struct {
 }
 
 func main() {
+    var api Api
+    api.Database = setupDatabase()
+    api.Router = setupRouter(api)
+
+    api.Router.Run(":8080")
+}
+
+func setupDatabase() *gorm.DB {
     db, err := gorm.Open(mysql.Open("cat-api:cat-api@tcp(127.0.0.1:3306)/cat-api?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
     if err != nil {
         panic("failed to connect database")
@@ -27,6 +36,51 @@ func main() {
 
     db.AutoMigrate(&Cat{})
 
-    r := setupRouter()
-    r.Run(":8080")
+    return db
+}
+
+func setupRouter(api Api) *gin.Engine {
+    r := gin.Default()
+
+    r.GET("/ping", func(c *gin.Context) {
+        c.String(200, "pong")
+    })
+
+    r.GET("/breeds", func(c *gin.Context) {
+        name := c.Query("name")
+        if name == "" {
+            c.String(400, "invalid breed name")
+            return
+        }
+
+        var cats []Cat
+        api.Database.Where("name LIKE ?", "%" + name + "%").Find(&cats)
+        if len(cats) == 0 {
+            cli := &http.Client{}
+            req, err := http.NewRequest("GET", "https://api.thecatapi.com/v1/breeds/search?q=" + name, nil)
+            if err != nil {
+                panic(err)
+            }
+            req.Header.Add("x-api-key", "DEMO-API-KEY")
+            res, err := cli.Do(req)
+            if err != nil {
+                panic(err)
+            }
+            defer res.Body.Close()
+            body, err := ioutil.ReadAll(res.Body)
+            if err != nil {
+                panic(err)
+            }
+            json.Unmarshal(body, &cats)
+            if len(cats) == 0 {
+                c.String(200, "no records")
+                return
+            }
+            api.Database.Create(&cats)
+        }
+
+        c.JSON(200, cats)
+    })
+
+    return r
 }
--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -8,13 +9,57 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func setup() Api {
+    var api Api
+    api.Database = setupDatabase()
+    api.Router = setupRouter(api)
+
+	return api
+}
+
 func TestPingRoute(t *testing.T) {
-	router := setupRouter()
+	api := setup()
 
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", "/ping", nil)
-	router.ServeHTTP(w, req)
+	api.Router.ServeHTTP(w, req)
 
 	assert.Equal(t, 200, w.Code)
 	assert.Equal(t, "pong", w.Body.String())
+}
+
+func TestBreedsRouteWithoutName(t *testing.T) {
+	api := setup()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/breeds", nil)
+	api.Router.ServeHTTP(w, req)
+
+	assert.Equal(t, 400, w.Code)
+	assert.Equal(t, "invalid breed name", w.Body.String())
+}
+
+func TestBreedsRouteWithoutRecords(t *testing.T) {
+	api := setup()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/breeds?name=foo", nil)
+	api.Router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "no records", w.Body.String())
+}
+
+func TestBreedsRouteWithNameSib(t *testing.T) {
+	api := setup()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/breeds?name=sib", nil)
+	api.Router.ServeHTTP(w, req)
+
+    var cats []Cat
+	json.Unmarshal(w.Body.Bytes(), &cats)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, 1, len(cats))
 }
